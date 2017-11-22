26417076b8aa928b6d7f53f782d76801b95de40a
SilverLaius <silver.laius@gmail.com>
2017-11-23 00:21:19 +0200

    Dogs!

--- main.py
+++ main.py
@@ -197,6 +197,146 @@ class HillBullet(sprite.Sprite):
 					player.hp -= 1
 				else:
 					player.hp = 1
+					
+class Dog(sprite.Sprite):
+	def __init__(self,x,y):
+		sprite.Sprite.__init__(self)
+		self.imagesR= [image.load("res/dogeRun1R.png").convert_alpha(),
+						image.load("res/dogeRun2R.png").convert_alpha(),
+						image.load("res/dogeRun3R.png").convert_alpha(),
+						image.load("res/dogeRun4R.png").convert_alpha(),
+						image.load("res/dogeRun5R.png").convert_alpha(),
+						image.load("res/dogeRun6R.png").convert_alpha()]
+		
+		self.imagesL= [image.load("res/dogeRun1L.png").convert_alpha(),
+						image.load("res/dogeRun2L.png").convert_alpha(),
+						image.load("res/dogeRun3L.png").convert_alpha(),
+						image.load("res/dogeRun4L.png").convert_alpha(),
+						image.load("res/dogeRun5L.png").convert_alpha(),
+						image.load("res/dogeRun6L.png").convert_alpha()]
+						
+		self.standimagesR= [image.load("res/dogeStand0R.png").convert_alpha(),
+							image.load("res/dogeStand1R.png").convert_alpha(),
+							image.load("res/dogeStand2R.png").convert_alpha(),
+							image.load("res/dogeStand3R.png").convert_alpha(),
+							image.load("res/dogeStand1R.png").convert_alpha(),
+							image.load("res/dogeStand2R.png").convert_alpha()]
+		
+		self.standimagesL= [image.load("res/dogeStand0L.png").convert_alpha(),
+							image.load("res/dogeStand1L.png").convert_alpha(),
+							image.load("res/dogeStand2L.png").convert_alpha(),
+							image.load("res/dogeStand3L.png").convert_alpha(),
+							image.load("res/dogeStand1L.png").convert_alpha(),
+							image.load("res/dogeStand2L.png").convert_alpha()]
+						
+		self.imagedict= {"imgR": self.imagesR, "imgL": self.imagesL, "imgRstand": self.standimagesR, "imgLstand": self.standimagesL}
+		self.index= 0
+		self.lugeja= 0
+		self.standing= True
+		self.state = "imgR"
+		self.image= self.imagedict["imgLstand"][self.index]
+		self.rect= self.image.get_rect().move((x,y))
+		self.onGround = False
+		self.dir = "left"
+		self.hp = 40
+		self.yvel=0
+		self.xvel=0
+		self.aggroArea = AggroRect(self, 500)
+		self.dying = False
+		
+	def update(self, platforms, player, billybullets, enemies):
+		if self.hp <= 0:
+			print("dead doggie")
+			#self.dying = True
+			#if self.state != "imgRdeath" and self.state != "imgLdeath":
+				#self.index = 0
+			#if self.dir == "right":
+				#self.state = "imgRdeath"
+			#elif self.dir == "left":
+				#self.state = "imgLdeath"
+		self.rect = self.rect.move(self.xvel, self.yvel)
+		if not self.onGround:
+			self.yvel += 0.3
+			if self.yvel > 80: self.yvel = 80
+		self.collide(self.xvel, 0, platforms, player, billybullets)
+		self.rect.top += self.yvel
+		self.onGround= False
+		self.collide(0, self.yvel, platforms, player, billybullets)
+		self.aggroArea.update(self)
+		self.collide(0, 0, platforms, player, billybullets)
+		self.animate(self.state, enemies)
+	
+	def animate(self, state, enemies):
+		if not self.dying:
+			self.lugeja+=1
+			if self.lugeja == 6:
+				if self.index != 5:
+					self.index+= 1
+				else:
+					self.index = 0
+				self.lugeja= 0
+		elif self.dying:
+			self.lugeja+=1
+			if self.lugeja == 6:
+				if self.index != 4:
+					self.index+= 1
+					self.lugeja= 0
+				else:
+					enemies.remove(self)		
+		self.image= self.imagedict[state][self.index]
+		
+	def collide(self, xvel, yvel, platforms, player, billybullets):
+		if not self.dying:
+			if xvel != 0 or yvel !=0:
+				for p in platforms:
+					if pygame.sprite.collide_rect(self, p):
+						if xvel > 0:
+							self.state = "imgL"
+							self.dir = "left"
+							self.image = self.imagedict[self.state][self.index]
+							self.rect.x -= 2
+							self.xvel = -1
+						if xvel < 0:
+							self.state = "imgR"
+							self.dir = "right"
+							self.image = self.imagedict[self.state][self.index]
+							self.xvel = 1
+							self.rect.x += 2
+						if yvel > 0:
+							self.rect.bottom = p.rect.top
+							self.onGround = True
+							self.yvel = 0
+						if yvel < 0:
+							self.rect.top = p.rect.bottom
+							self.yvel += 1
+			else:
+				if pygame.sprite.collide_rect(self.aggroArea, player):
+					deltax = player.rect.x - self.rect.x-38
+					if deltax > 0:
+						self.standing = False
+						self.xvel = 3
+						self.state = "imgR"
+						self.dir = "right"
+					else:
+						self.standing = False
+						self.xvel = -3
+						self.state = "imgL"
+						self.dir = "left"
+				else:
+					if self.dir == "right":
+						self.standing = True
+						self.xvel = 0
+						self.state = "imgRstand"
+					elif self.dir == "left":
+						self.standing = True
+						self.xvel = 0
+						self.state = "imgLstand"
+			
+			if pygame.sprite.collide_rect(self, player):
+				if self.dir == "right":
+					player.rect.x+= 3
+				elif self.dir == "left":
+					player.rect.x-=3
 
 class Hillbilly(sprite.Sprite):
 	def __init__(self,x,y):
@@ -241,7 +381,6 @@ class Hillbilly(sprite.Sprite):
 		self.xvel=1
 		self.aggroArea = AggroRect(self, 500)
 		self.dying = False
-		self.lastxvel = 1
 		self.reload = 0
 		self.shootsound = pygame.mixer.Sound('res/Gunshot2.wav')
 		
@@ -332,11 +471,11 @@ class Hillbilly(sprite.Sprite):
 					self.reload = 79
 					if self.dir == "right":
 						self.standing = False
-						self.xvel = 2
+						self.xvel = 4
 						self.state = "imgR"
 					elif self.dir == "left":
 						self.standing = False
-						self.xvel = -2
+						self.xvel = -4
 						self.state = "imgL"
 			
 			if pygame.sprite.collide_rect(self, player):
@@ -858,6 +997,9 @@ def gen_world(filename):
 			if(b==150):
 				hillbilly = Hillbilly(i*32,j*32)
 				enemies.add(hillbilly)
+			if(r==205 and g==205):
+				dog = Dog(i*32,j*32)
+				enemies.add(dog)
 			
 	newlist.append(entities)
 	newlist.append(player)
